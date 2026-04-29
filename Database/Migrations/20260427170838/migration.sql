ca65381e0fea43dfe7e4d0cf883abf7b5cd08d7f
ShikiHTM <kizukusan123@gmail.com>
2026-04-29 07:18:04 +0700

    feat (database): add verifyToken

---
+++ Database/Migrations/20260427170838/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[verifyToken]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "verifyToken" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_verifyToken_key" ON "User"("verifyToken");
