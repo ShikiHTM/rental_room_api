9020ed6eb3531db455e9c9fe20c736cc71235903
ShikiHTM <kizukusan123@gmail.com>
2026-05-07 23:50:09 +0700

    feat (auth): add CreateUserSchema and LoginUserSchema

--- Utils/schemas/user.schema.ts
+++ Utils/schemas/user.schema.ts
@@ -1,9 +1,14 @@
 import { z } from "zod";
 
-export const UpdateUserSchema = z.object({
-    fullName: z.string().min(2).max(100).optional(),
-    phoneNumber: z.string().max(20).optional(),
-    email: z.string().email().optional(),
-});
+export const CreateUserSchema = z.object({
+    fullName: z.string().min(2).max(100),
+    phoneNumber: z.string().max(20),
+    email: z.email(),
+    password: z.string()
+})
+
+export const UpdateUserSchema = CreateUserSchema.omit({ password: true }).partial();
+export const LoginUserSchema = CreateUserSchema.pick({ email: true, password: true })
 
 export type UpdateUserInput = z.infer<typeof UpdateUserSchema>;
+export type CreateUserInput = z.infer<typeof CreateUserSchema>;
